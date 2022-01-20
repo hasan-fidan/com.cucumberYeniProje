d22c94616682e7ca9a7fc3191ee2a0dab42f53ad
Hasan F <hfidan006@gmail.com>
2022-01-20 21:39:51 +0100

    first

--- com.MavenBatch44/src/test/java/day05_mavenProjeOlusturma/C01_FirstMavenClass.java
+++ com.MavenBatch44/src/test/java/day05_mavenProjeOlusturma/C01_FirstMavenClass.java
@@ -1,4 +1,28 @@
 package day05_mavenProjeOlusturma;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class C01_FirstMavenClass {
+    public static void main(String[] args) {
+
+
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+
+
+
+
+
+
+
+    }
 }

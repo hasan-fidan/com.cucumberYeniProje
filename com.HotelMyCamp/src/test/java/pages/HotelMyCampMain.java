e68e9a957388d3ae84bc2fe29c47fe446c937fc0
Hasan F <hfidan006@gmail.com>
2022-02-09 15:45:41 +0100

    09.02

---
+++ com.HotelMyCamp/src/test/java/pages/HotelMyCampMain.java
@@ -0,0 +1,20 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+
+public class HotelMyCampMain {
+
+    public HotelMyCampMain() {
+
+        PageFactory.initElements();
+
+    }
+
+
+
+
+
+
+
+
+}
